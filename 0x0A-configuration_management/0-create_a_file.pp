4295bbdfeb9e184ec1259d5dbd1d1e403144084e
Precious Aleaji <legenduz15@gmail.com>
2024-01-23 04:36:14 +0100

    0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates file at /tmp/school with specific permissions
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
