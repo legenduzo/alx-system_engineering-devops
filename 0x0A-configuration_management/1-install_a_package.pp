6a38873c29ae3bd4d9982aa25cb861363ab24259
Precious Aleaji <legenduz15@gmail.com>
2024-01-23 05:54:43 +0100

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# installs flask
+# install Flask v2.1.0  using pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
